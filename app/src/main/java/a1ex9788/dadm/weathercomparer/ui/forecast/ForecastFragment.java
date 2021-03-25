9fa613b16a1d24d59d3bce36b38935a0c2c6ed93
guiors <orsetprele@eisti.eu>
2021-03-25 18:53:34 +0100

    Modificaciones de los ficheros en torno a PredictionFragment y prediction_layout para poner las animaciones. He creado una carpeta "RAW" para poner las animaciones en JSON pero por el momento he puesto el enlace "URL" de los animaciones ya que no consiguo a poner directamente los ficheros JSON.
    Además, he modificado los ficheros Strings, Color.
    Por el momento, he puesto el valor de ConditionWeather en una variable de tipo String antes de poner los reales datos.

--- app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
+++ app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
@@ -12,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.HashMap;
 import java.util.List;
 
 import a1ex9788.dadm.weathercomparer.R;
@@ -26,16 +29,68 @@ public class ForecastFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        //Dictionary for catching the value of WeatherCondition
+        HashMap<String, String> dicWeatherCondition = new HashMap<String, String>();
+        dicWeatherCondition.put("ThunderstormWithRain","https://assets7.lottiefiles.com/temp/lf20_XkF78Y.json");
+        dicWeatherCondition.put("Thunderstorm","https://assets7.lottiefiles.com/temp/lf20_Kuot2e.json");
+        dicWeatherCondition.put("Fog","https://assets7.lottiefiles.com/temp/lf20_kOfPKE.json");
+        dicWeatherCondition.put("Rain","https://assets6.lottiefiles.com/temp/lf20_rpC1Rd.json");
+        dicWeatherCondition.put("HeavyRain","https://assets4.lottiefiles.com/packages/lf20_bgmkqg6b.json");
+        dicWeatherCondition.put("SnowRain","https://assets4.lottiefiles.com/packages/lf20_uhdaqfkg.json");
+        dicWeatherCondition.put("LightSnow","https://assets7.lottiefiles.com/temp/lf20_BSVgyt.json");
+        dicWeatherCondition.put("Snow","https://assets7.lottiefiles.com/temp/lf20_WtPCZs.json");
+        dicWeatherCondition.put("Flurries","https://assets9.lottiefiles.com/packages/lf20_J8EPQ1.json");
+        dicWeatherCondition.put("Clouds","https://assets7.lottiefiles.com/temp/lf20_VAmWRg.json");
+        dicWeatherCondition.put("UnknownPrecipitation","https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json");
+        dicWeatherCondition.put("Sunny","https://assets7.lottiefiles.com/temp/lf20_Stdaec.json");
+
         forecastViewModel =
                 new ViewModelProvider(this).get(ForecastViewModel.class);
         View root = inflater.inflate(R.layout.fragment_forecast, container, false);
-        final TextView textView = root.findViewById(R.id.text_forecast);
-        forecastViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        //All the different cases of WeatherCondition
+        final LottieAnimationView animationView = root.findViewById(R.id.animationViewWeather);
+        String conditionWeather ="ThunderstormWithRain";
+        switch (conditionWeather) {
+            case "Sunny":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Sunny"));
+                break;
+            case "UnknownPrecipitation":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("UnknownPrecipitation"));
+                break;
+            case "Clouds":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Clouds"));
+                break;
+            case "Flurries":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Flurries"));
+                break;
+            case "Snow":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Snow"));
+                break;
+            case "LightSnow":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("LightSnow"));
+                break;
+            case "SnowRain":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("SnowRain"));
+                break;
+            case "HeavyRain":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("HeavyRain"));
+                break;
+            case "Rain":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Rain"));
+                break;
+            case "Fog":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Fog"));
+                break;
+            case "Thunderstorm":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("Thunderstorm"));
+                break;
+            case "ThunderstormWithRain":
+                animationView.setAnimationFromUrl(dicWeatherCondition.get("ThunderstormWithRain"));
+
+                break;
+        }
+
         return root;
     }
 
