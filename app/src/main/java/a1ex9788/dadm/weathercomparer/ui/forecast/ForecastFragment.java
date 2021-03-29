10a16ea05e0f0b12f72b79c80c2190270ac2e8b5
Alejandro <alejandrocarrion1999@gmail.com>
2021-03-29 16:17:53 +0200

    Separar lógica de Forecast en ForecastViewModel.

--- app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
+++ app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
@@ -4,26 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.HashMap;
-import java.util.List;
 
 import a1ex9788.dadm.weathercomparer.R;
-import a1ex9788.dadm.weathercomparer.model.DayForecast;
-import a1ex9788.dadm.weathercomparer.model.HourForecast;
-import a1ex9788.dadm.weathercomparer.webServices.forecasts.WeatherForecast;
-import a1ex9788.dadm.weathercomparer.webServices.forecasts.accuWeather.AccuWeatherForecast;
 
 public class ForecastFragment extends Fragment {
 
@@ -32,28 +24,31 @@ public class ForecastFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        //Dictionary for catching the value of WeatherCondition
+        new GetDayForecastThread().start();
+        new GetHourForecastThread().start();
+
+        // Dictionary for catching the value of WeatherCondition
         HashMap<String, String> dicWeatherCondition = new HashMap<String, String>();
-        dicWeatherCondition.put("ThunderstormWithRain","https://assets7.lottiefiles.com/temp/lf20_XkF78Y.json");
-        dicWeatherCondition.put("Thunderstorm","https://assets7.lottiefiles.com/temp/lf20_Kuot2e.json");
-        dicWeatherCondition.put("Fog","https://assets7.lottiefiles.com/temp/lf20_kOfPKE.json");
-        dicWeatherCondition.put("Rain","https://assets6.lottiefiles.com/temp/lf20_rpC1Rd.json");
-        dicWeatherCondition.put("HeavyRain","https://assets4.lottiefiles.com/packages/lf20_bgmkqg6b.json");
-        dicWeatherCondition.put("SnowRain","https://assets4.lottiefiles.com/packages/lf20_uhdaqfkg.json");
-        dicWeatherCondition.put("LightSnow","https://assets7.lottiefiles.com/temp/lf20_BSVgyt.json");
-        dicWeatherCondition.put("Snow","https://assets7.lottiefiles.com/temp/lf20_WtPCZs.json");
-        dicWeatherCondition.put("Flurries","https://assets9.lottiefiles.com/packages/lf20_J8EPQ1.json");
-        dicWeatherCondition.put("Clouds","https://assets7.lottiefiles.com/temp/lf20_VAmWRg.json");
-        dicWeatherCondition.put("UnknownPrecipitation","https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json");
-        dicWeatherCondition.put("Sunny","https://assets7.lottiefiles.com/temp/lf20_Stdaec.json");
+        dicWeatherCondition.put("ThunderstormWithRain", "https://assets7.lottiefiles.com/temp/lf20_XkF78Y.json");
+        dicWeatherCondition.put("Thunderstorm", "https://assets7.lottiefiles.com/temp/lf20_Kuot2e.json");
+        dicWeatherCondition.put("Fog", "https://assets7.lottiefiles.com/temp/lf20_kOfPKE.json");
+        dicWeatherCondition.put("Rain", "https://assets6.lottiefiles.com/temp/lf20_rpC1Rd.json");
+        dicWeatherCondition.put("HeavyRain", "https://assets4.lottiefiles.com/packages/lf20_bgmkqg6b.json");
+        dicWeatherCondition.put("SnowRain", "https://assets4.lottiefiles.com/packages/lf20_uhdaqfkg.json");
+        dicWeatherCondition.put("LightSnow", "https://assets7.lottiefiles.com/temp/lf20_BSVgyt.json");
+        dicWeatherCondition.put("Snow", "https://assets7.lottiefiles.com/temp/lf20_WtPCZs.json");
+        dicWeatherCondition.put("Flurries", "https://assets9.lottiefiles.com/packages/lf20_J8EPQ1.json");
+        dicWeatherCondition.put("Clouds", "https://assets7.lottiefiles.com/temp/lf20_VAmWRg.json");
+        dicWeatherCondition.put("UnknownPrecipitation", "https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json");
+        dicWeatherCondition.put("Sunny", "https://assets7.lottiefiles.com/temp/lf20_Stdaec.json");
 
         forecastViewModel =
                 new ViewModelProvider(this).get(ForecastViewModel.class);
         View root = inflater.inflate(R.layout.fragment_forecast, container, false);
 
-        //All the different cases of WeatherCondition
+        // All the different cases of WeatherCondition
         final LottieAnimationView animationView = root.findViewById(R.id.animationViewWeather);
-        String conditionWeather ="ThunderstormWithRain";
+        String conditionWeather = "ThunderstormWithRain";
         switch (conditionWeather) {
             case "Sunny":
                 animationView.setAnimationFromUrl(dicWeatherCondition.get("Sunny"));
@@ -100,24 +95,25 @@ public class ForecastFragment extends Fragment {
         return root;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        new MyThread().start();
-    }
-
-    private class MyThread extends Thread {
+    private class GetDayForecastThread extends Thread {
 
         @Override
         public void run() {
             try {
-                WeatherForecast weatherForecast = new AccuWeatherForecast(39.289, -0.799);
-                //WeatherForecast weatherForecast = new OpenWeatherForecast(39.289, -0.799);
-                //WeatherForecast weatherForecast = new WeatherBitForecast(39.289, -0.799);
-                List<DayForecast> dailyForecast = weatherForecast.getDailyForecast();
-                List<HourForecast> hourlyForecast = weatherForecast.getHourlyForecast();
-                int nothing = 0;
+                forecastViewModel.getAccuWeatherDailyForecast();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class GetHourForecastThread extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                forecastViewModel.getAccuWeatherHourlyForecast();
             } catch (Exception e) {
                 e.printStackTrace();
             }
