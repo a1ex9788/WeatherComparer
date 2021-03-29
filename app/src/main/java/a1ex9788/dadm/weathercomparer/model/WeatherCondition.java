25f061efaea9176288d06539e614c40202c10667
Alejandro <alejandrocarrion1999@gmail.com>
2021-03-29 18:49:00 +0200

    Refactor WeatherCondition.

--- app/src/main/java/a1ex9788/dadm/weathercomparer/model/WeatherCondition.java
+++ app/src/main/java/a1ex9788/dadm/weathercomparer/model/WeatherCondition.java
@@ -1,5 +1,31 @@
 package a1ex9788.dadm.weathercomparer.model;
 
 public enum WeatherCondition {
-    Clear, Clouds, BrokenClouds, Rain, Snow, Fog, Flurries, Thunderstorm, ThunderstormWithRain, UnknownPrecipitation
+    // TODO: Substitute the strings of the weather conditions for a resource in order to enable the language change.
+    Clear("Clear", "https://assets7.lottiefiles.com/temp/lf20_Stdaec.json"),
+    Clouds("Clouds", "https://assets7.lottiefiles.com/temp/lf20_VAmWRg.json"),
+    BrokenClouds("Broken clouds", "https://assets5.lottiefiles.com/temp/lf20_dgjK9i.json"),
+    Rain("Rain", "https://assets6.lottiefiles.com/temp/lf20_rpC1Rd.json"),
+    Snow("Snow", "https://assets7.lottiefiles.com/temp/lf20_WtPCZs.json"),
+    Fog("Fog", "https://assets7.lottiefiles.com/temp/lf20_kOfPKE.json"),
+    Flurries("Flurries", "https://assets9.lottiefiles.com/packages/lf20_J8EPQ1.json"),
+    Thunderstorm("Thunderstorm", "https://assets7.lottiefiles.com/temp/lf20_Kuot2e.json"),
+    ThunderstormWithRain("Thunderstorm with rain", "https://assets7.lottiefiles.com/temp/lf20_XkF78Y.json"),
+    UnknownPrecipitation("Unknown precipitation", "https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json"),
+    Unknown("Unknown", "https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json");
+
+    private String text, iconAddress;
+
+    WeatherCondition(String text, String iconAddress) {
+        this.text = text;
+        this.iconAddress = iconAddress;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getIconAddress() {
+        return iconAddress;
+    }
 }
--- app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
+++ app/src/main/java/a1ex9788/dadm/weathercomparer/ui/forecast/ForecastFragment.java
@@ -8,13 +8,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.airbnb.lottie.LottieAnimationView;
-
-import java.util.HashMap;
 
 import a1ex9788.dadm.weathercomparer.MainActivity;
 import a1ex9788.dadm.weathercomparer.R;
@@ -23,85 +20,25 @@ import a1ex9788.dadm.weathercomparer.model.WeatherCondition;
 public class ForecastFragment extends Fragment {
 
     private ForecastViewModel forecastViewModel;
-    private ConstraintLayout clBottomSheet;
     private ImageButton ibNavigationDrawer;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        forecastViewModel =
-                new ViewModelProvider(this).get(ForecastViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        forecastViewModel = new ViewModelProvider(this).get(ForecastViewModel.class);
 
         new GetDayForecastThread().start();
         new GetHourForecastThread().start();
 
-        // Dictionary for catching the value of WeatherCondition
-        HashMap<WeatherCondition, String> dicWeatherCondition = new HashMap();
-        dicWeatherCondition.put(WeatherCondition.Clear, "https://assets7.lottiefiles.com/temp/lf20_Stdaec.json");
-        dicWeatherCondition.put(WeatherCondition.Clouds, "https://assets7.lottiefiles.com/temp/lf20_VAmWRg.json");
-        dicWeatherCondition.put(WeatherCondition.BrokenClouds, "https://assets5.lottiefiles.com/temp/lf20_dgjK9i.json");
-        dicWeatherCondition.put(WeatherCondition.Rain, "https://assets6.lottiefiles.com/temp/lf20_rpC1Rd.json");
-        dicWeatherCondition.put(WeatherCondition.Snow, "https://assets7.lottiefiles.com/temp/lf20_WtPCZs.json");
-        dicWeatherCondition.put(WeatherCondition.Fog, "https://assets7.lottiefiles.com/temp/lf20_kOfPKE.json");
-        dicWeatherCondition.put(WeatherCondition.Flurries, "https://assets9.lottiefiles.com/packages/lf20_J8EPQ1.json");
-        dicWeatherCondition.put(WeatherCondition.Thunderstorm, "https://assets7.lottiefiles.com/temp/lf20_Kuot2e.json");
-        dicWeatherCondition.put(WeatherCondition.ThunderstormWithRain, "https://assets7.lottiefiles.com/temp/lf20_XkF78Y.json");
-        dicWeatherCondition.put(WeatherCondition.UnknownPrecipitation, "https://assets10.lottiefiles.com/temp/lf20_9gY9Yf.json");
-
-        View root = inflater.inflate(R.layout.fragment_forecast, container, false);
-
-        // All the different cases of WeatherCondition
+        WeatherCondition weatherCondition = WeatherCondition.Fog;
         final LottieAnimationView animationView = root.findViewById(R.id.animationViewWeather);
         final TextView mWeatherTextView = root.findViewById(R.id.weatherTextView);
-        WeatherCondition weatherCondition = WeatherCondition.Clear;
-        // TODO: Substitute the strings of the weather conditions for a resource in order to enable the language change.
-        switch (weatherCondition) {
-            case Clear:
-                mWeatherTextView.setText("Sunny");
-                break;
-            case Clouds:
-                mWeatherTextView.setText("Cloudy");
-                break;
-            case BrokenClouds:
-                mWeatherTextView.setText("Broken Clouds");
-                break;
-            case Rain:
-                mWeatherTextView.setText("Rainy");
-                break;
-            case Snow:
-                animationView.setSpeed((float) 1.8);
-                mWeatherTextView.setText("Snowy");
-                break;
-            case Fog:
-                mWeatherTextView.setText("Foggy");
-                break;
-            case Flurries:
-                mWeatherTextView.setText("Flurries");
-                break;
-            case Thunderstorm:
-                mWeatherTextView.setText("Stormy");
-                break;
-            case ThunderstormWithRain:
-                mWeatherTextView.setText("Stormy and Rainy");
-                break;
-            case UnknownPrecipitation:
-                mWeatherTextView.setText("Unknown Precipitation");
-                break;
-
-            default:
-                weatherCondition = WeatherCondition.UnknownPrecipitation;
-                mWeatherTextView.setText("Unknown");
-                break;
-        }
-
-        animationView.setAnimationFromUrl(dicWeatherCondition.get(weatherCondition));
+        mWeatherTextView.setText(weatherCondition.getText());
+        animationView.setAnimationFromUrl(weatherCondition.getIconAddress());
 
         this.ibNavigationDrawer = root.findViewById(R.id.ibNavigationDrawer);
-        this.ibNavigationDrawer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((MainActivity) getActivity()).openNavigationDrawer();
-            }
-        });
+        this.ibNavigationDrawer.setOnClickListener(v -> ((MainActivity) getActivity()).openNavigationDrawer());
 
         return root;
     }
